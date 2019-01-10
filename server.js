d708dff5a4c4b735db14d5e5daa1a292f7cce342
owitiA <owitigracie@gmail.com>
2019-01-10 10:52:02 +0300

    test run

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 'use strict '
 // import {logOnRequest} from 'pino-wrapper';
 // const app = require('express')();
-
-
 // app.use(logOnRequest());
-
 //import pino from 'pino';
 import app from './src/app';
 import expressPino from 'express-pino-logger'
+import { request } from 'https';
 
 app.use(expressPino)
+
+app.get('/', (req, res) => {
+    req.log.error(Error('run this test'))
+    res.send('hope it works right')
+})
+
+app.listen(3000)
+
